ba66b581ca43aeba20d7ac79dc69ac81b357ed06
Rohit Roy <rohhiiiit@gmail.com>
2022-12-09 21:05:48 +0530

    Update Decentragram.sol

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,40 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+  string public name = "Decentragram";
+
+  // Store Images
+  uint public imageCount = 0;
+  mapping(uint => Image) public images;
+
+  struct Image {
+    uint id;
+    string hash;
+    string description;
+    uint tipAmount;
+    address payable author;
+  }
+
+  event ImageCreated(
+    uint id,
+    string hash,
+    string description,
+    uint tipAmount,
+    address payable author
+  );
+
+  //Create Images
+  function uploadImage(string memory _imgHash,string memory _description) public {
+    //Increment image id
+    imageCount = imageCount ++;
+    
+    //Add image to contract
+    images[imageCount] = Image(imageCount,_imgHash,_description,0,msg.sender);
+
+    //Trigger an event
+    emit ImageCreated(imageCount, _imghash, _description, 0, msg.sender);
+  }
+  
+  // Tip Images
+  
 }
